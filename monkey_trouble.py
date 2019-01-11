3530c75afe8c0f6b6cfb6bae220cd5d4cd142a1b
lugasyk <lugassyk@gmail.com>
2019-01-11 17:33:32 +0100

    Delete monkey_trouble.py
    committed by mistake

--- monkey_trouble.py
+++
@@ -1,7 +0,0 @@
-def monkey_troube(a_smile, b_smile):
-    while True:
-        if a_smile and b_smile:
-            return True
-        if not a_smile and not b_smile:
-            return True
-        return False

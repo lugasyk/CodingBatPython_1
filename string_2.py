36b4daa9ff2554b2dc35a9c2d2e298b853bd322c
lugasyk <lugassyk@gmail.com>
2019-01-13 18:51:09 +0100

    add solution for a few the following section string_2

---
+++ string_2.py
@@ -0,0 +1,33 @@
+#double char
+def double_char(str):
+    return ''.join([char*2 for char in str])
+
+#count hi
+def count_hi(str):
+  return str.count("hi")
+
+#count cat and dog
+def cat_dog(str):
+  catcount = str.count("cat")
+  dogcount = str.count("dog")
+  if catcount == dogcount:
+    return True
+  else:
+    return False
+
+# count_code
+def count_code(str):
+  count = 0
+  i=0
+  while "co" in str[i:]:
+    if len(str[i+str[i:].index("co"):]) >= 4 and str[i+3+str[i:].index("co")] == "e":
+      count += 1
+    i += str[i:].index("co")+3
+  return count
+
+# end_other
+def end_other(a, b):
+  long_s, short_s = (a,b) if len(a) >= len(b) else (b,a)
+  return long_s.lower().endswith(short_s.lower())
+
+#
